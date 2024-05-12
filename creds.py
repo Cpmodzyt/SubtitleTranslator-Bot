9dc2b5b804b8ab3e20dd0afdc6e1db0c2c3601d0
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-05-12 15:44:19 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -6,11 +6,11 @@ load_dotenv(find_dotenv())
 
 class cred:
     BOT_NAME = os.getenv("BOT_NAME")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")  # From botfather
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "6916875347:AAHYnAt4IERVVcvNCg3eeRljreEoWbbQOe4")  # From botfather
     API_ID = os.getenv(
-        "API_ID"
+        "API_ID", "10471716"
     )  # "Get this value from my.telegram.org! Please do not steal"
     API_HASH = os.getenv(
-        "API_HASH"
+        "API_HASH", "f8a1b21a13af154596e2ff5bed164860"
     )  # "Get this value from my.telegram.org! Please do not steal"
-    DB_URL = os.getenv("DB_URL")  # From Firebase database
+    DB_URL = os.getenv("DB_URL", "https://telegram-bot-51cbb-default-rtdb.firebaseio.com")  # From Firebase database
