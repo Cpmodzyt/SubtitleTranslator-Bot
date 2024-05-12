e2371ea845f83dec21371b1b6f08f2a0c499abd7
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-05-12 15:45:37 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -4,13 +4,9 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 
-class cred:
-    BOT_NAME = os.getenv("BOT_NAME")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "6916875347:AAHYnAt4IERVVcvNCg3eeRljreEoWbbQOe4")  # From botfather
-    API_ID = os.getenv(
-        "API_ID", "10471716"
-    )  # "Get this value from my.telegram.org! Please do not steal"
-    API_HASH = os.getenv(
-        "API_HASH", "f8a1b21a13af154596e2ff5bed164860"
-    )  # "Get this value from my.telegram.org! Please do not steal"
-    DB_URL = os.getenv("DB_URL", "https://telegram-bot-51cbb-default-rtdb.firebaseio.com")  # From Firebase database
+class cred():
+    BOT_TOKEN = "6916875347:AAHYnAt4IERVVcvNCg3eeRljreEoWbbQOe4"
+    API_ID = "10471716"       
+    API_HASH = "f8a1b21a13af154596e2ff5bed164860"   
+    DB_URL = "https://telegram-bot-51cbb-default-rtdb.firebaseio.com" 
+    BOT_NAME = "ioTester"     
