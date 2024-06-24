f7442a9be21e48a6b65e274a8914d1d43eeece72
Appu John <161853537+appujohn007@users.noreply.github.com>
2024-06-24 09:31:23 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -5,7 +5,7 @@ load_dotenv(find_dotenv())
 
 
 class cred():
-    BOT_TOKEN = "6916875347:AAHYnAt4IERVVcvNCg3eeRljreEoWbbQOe4"
+    BOT_TOKEN = "6916875347:AAEVxR4cO_sIBB6V57ANA92pHKxzw9G3yX0"
     API_ID = "10471716"       
     API_HASH = "f8a1b21a13af154596e2ff5bed164860"   
     DB_URL = "https://telegram-bot-51cbb-default-rtdb.firebaseio.com" 
